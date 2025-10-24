4c27a77d433ab4a5fe665a4c407c03d48fc78239
jxhnhxrvyx11-web <jxhnhxrvyx11@gmail.com>
2025-10-24 22:28:10 +0800

    Update Playlist.java

--- player/Playlist.java
+++ player/Playlist.java
@@ -1,1 +1,27 @@
 
+package media;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Playlist extends PlaylistComponent {
+    private String name;
+    private List<PlaylistComponent> components = new ArrayList<>();
+
+    public Playlist(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void add(PlaylistComponent c) {
+        components.add(c);
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playlist: " + name);
+        for (PlaylistComponent c : components) {
+            c.play();
+        }
+    }
+}
