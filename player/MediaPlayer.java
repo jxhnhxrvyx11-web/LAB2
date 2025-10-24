3e8ff51911e3ac0e4a5bc2dd404d135f9cae0d18
jxhnhxrvyx11-web <jxhnhxrvyx11@gmail.com>
2025-10-24 22:27:40 +0800

    Update MediaPlayer.java

--- player/MediaPlayer.java
+++ player/MediaPlayer.java
@@ -1,1 +1,20 @@
+package media;
+
+public class MediaPlayer {
+    private Renderer renderer;
+
+    public MediaPlayer(Renderer renderer) {
+        this.renderer = renderer;
+    }
+
+    public void setRenderer(Renderer renderer) {
+        this.renderer = renderer;
+    }
+
+    public void play(PlaylistComponent component) {
+        renderer.render("Starting playback...");
+        component.play();
+        renderer.render("Playback finished.");
+    }
+}
 
