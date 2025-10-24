0af97ac6e15a2ff12e545a4fe076ed3e385ff881
jxhnhxrvyx11-web <jxhnhxrvyx11@gmail.com>
2025-10-24 22:20:42 +0800

    Update LocalFileAdapter.java

--- localfile/LocalFileAdapter.java
+++ localfile/LocalFileAdapter.java
@@ -1,1 +1,25 @@
+package media;
+
+public class LocalFileAdapter implements IMediaSource {
+    private String fileName;
+
+    public LocalFileAdapter(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void open() {
+        System.out.println("Opening local file: " + fileName);
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing local file: " + fileName);
+    }
+
+    @Override
+    public void close() {
+        System.out.println("Closing local file: " + fileName);
+    }
+}
 
