b8af653f7d82e9d871773ea8ca8c5693d8a93361
jxhnhxrvyx11-web <jxhnhxrvyx11@gmail.com>
2025-10-24 22:16:11 +0800

    Update MediaDecorator.java

--- proxy/MediaDecorator.java
+++ proxy/MediaDecorator.java
@@ -1,1 +1,11 @@
+package media;
 
+public abstract class MediaDecorator extends PlaylistComponent {
+    protected PlaylistComponent component;
+
+    public MediaDecorator(PlaylistComponent component) {
+        this.component = component;
+    }
+
+    public abstract void play();
+}
